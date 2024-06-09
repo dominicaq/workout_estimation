c775fef0c681118b880ebc3ff33b85b549e5b35d
josegav7 <jose_gp96@hotmail.com>
2024-06-08 17:49:29 -0700

    refactor

--- filter_data.py
+++ filter_data.py
@@ -8,9 +8,7 @@ def filter_data():
     train_annotations = pd.read_csv(r'k700-2020\annotations\train.csv')
     val_annotations = pd.read_csv(r'k700-2020\annotations\val.csv')
 
-    #classes = ['squat', 'deadlifting', 'bench pressing']
-    #classes = ['push up']
-    classes = ['squat', 'deadlifting', 'bench pressing', 'push up']
+    classes = ['squat', 'deadlifting', 'push up']
 
     filtered_train = train_annotations[train_annotations['label'].isin(classes)]
     filtered_val = val_annotations[val_annotations['label'].isin(classes)]
